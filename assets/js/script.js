90198a4a65ff3cc70fa1780fdffe5e95dcc7b7c4
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-06-10 22:42:15 -0600

    added variables to handle inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const titleInput = $("#taskTitle");
 const dateInput = $("#taskDeadline");
+const descInput = $("#taskDescription");
+const submitTask = $("#saveTaskButton");
 
 // TODO: create a function to generate a unique task id
 function generateTaskId() {
@@ -9,13 +12,17 @@ function generateTaskId() {
 }
 
 // TODO: create a function to create a task card
-function createTaskCard(task) {}
+function createTaskCard(task) {
+  return task;
+}
 
 // TODO: create a function to render the task list and make cards draggable
 function renderTaskList() {}
 
 // TODO: create a function to handle adding a new task
-function handleAddTask(event) {}
+function handleAddTask(event) {
+  event.preventDefault();
+}
 
 // TODO: create a function to handle deleting a task
 function handleDeleteTask(event) {}
@@ -26,4 +33,5 @@ function handleDrop(event, ui) {}
 // TODO: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
   dateInput.datepicker();
+  //   submitTask.submit(handleAddTask);
 });
