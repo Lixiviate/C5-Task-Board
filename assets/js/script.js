6ddab5984cd44f464606da8eef40bd47f092090e
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-06-11 22:57:26 -0600

    Updated create task card.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 const titleInput = $("#taskTitle");
 const dateInput = $("#taskDeadline");
 const descInput = $("#taskDescription");
-const submitTask = $("#saveTaskButton");
+const taskForm = $("#taskForm");
 
 // TODO: create a function to generate a unique task id
 function generateTaskId() {
@@ -13,7 +13,15 @@ function generateTaskId() {
 
 // TODO: create a function to create a task card
 function createTaskCard(task) {
-  return task;
+  const card = $("<div>").addClass("card");
+  const cardBody = $("<div>").addClass("card-body");
+  const cardTitle = $("<div>").addClass("card-title").text(task.title);
+  const cardDate = $("<div>").addClass("card-subtitle").text(task.date);
+  const cardDisc = $("<div>").addClass("card-text").text(task.desc);
+
+  cardBody.append(cardTitle, cardDate, cardDisc);
+  card.append(cardBody);
+  card.appendTo("#todo-cards");
 }
 
 // TODO: create a function to render the task list and make cards draggable
@@ -33,5 +41,6 @@ function handleDrop(event, ui) {}
 // TODO: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
   dateInput.datepicker();
-  //   submitTask.submit(handleAddTask);
+  renderTaskList();
+  taskForm.on("submit", handleAddTask);
 });
